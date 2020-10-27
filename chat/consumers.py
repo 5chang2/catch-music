201e51a8458a42b3e79413b086877551ac60ca9a
bing <qlstks@gmail.com>
2020-10-27 21:02:00 +0900

    user enter and leave completed

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,44 +1,92 @@
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Room, User
 import json
 
-class ChatConsumer(WebsocketConsumer):
-  	# websocket 연결 시 실행
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+  	
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
-        self.is_solved = False
-        async_to_sync(self.channel_layer.group_add)(
+        self.room_group_name = self.room_name
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 	# websocket 연결 종료 시 실행 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.delete_user(self.user.id)
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 	# 클라이언트로부터 메세지를 받을 시 실행
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        if (self.is_solved == False) and (message=='hello'):
-            message='hello, 정답입니다!'
-            self.is_solved = True
-				# 클라이언트로부터 받은 메세지를 다시 클라이언트로 보내준다.
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+        action = text_data_json.get('action')
+        username = text_data_json.get('username')
+        message = text_data_json.get('message')
+        if action == 'enter':
+            room, user_count = await self.get_room_or_create(self.room_name)
+            # room에 아무도 없다면
+            if user_count == 0:
+                user = await self.create_user(username, True, room)
+            else:
+                user = await self.create_user(username, False, room)
+            self.user = user
+            await self.send(text_data=json.dumps({
+                'action': 'enter',
+                'isHost': user.is_host,
+                'userList': await self.get_user_list(self.room_name),
+            }))
+        elif action == 'message':
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat.message',
+                    'message': message
+                }
+            )
+        elif action == 'start':
+            pass
+        else:
+            pass
     
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
         # WebSocket 에게 메세지 전송
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
+    
+    @database_sync_to_async
+    def get_room_or_create(self, room_name):
+        room = Room.objects.get_or_create(room_name=room_name)
+        user_count = room[0].user_set.count()
+        return (room[0], user_count)
+
+
+    @database_sync_to_async
+    def create_user(self, username, is_host, room):
+        user = User(is_host=is_host, username=username, room=room)
+        user.save()
+        return user
+
+
+    @database_sync_to_async
+    def get_user(self, username):
+        user = User.objects.get(username=username)
+        return user
+    
+    @database_sync_to_async
+    def get_user_list(self, room_name):
+        room = Room.objects.get(room_name=room_name)
+        user_list = [member for member in room.user_set.values()]
+        return user_list
+    
+
+    @database_sync_to_async
+    def delete_user(self, user_id):
+        user = User.objects.get(pk=user_id)
+        user.delete()
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    room_name = models.CharField(max_length=100)
+
+
+class User(models.Model):
+    is_host = models.BooleanField()
+    username = models.CharField(max_length=100)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
